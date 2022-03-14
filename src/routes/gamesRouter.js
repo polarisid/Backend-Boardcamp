1f9e719d0c68b1a56d4ae698618581f17ee19588
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-03-14 10:52:35 -0300

    Preparando revisão de código

--- src/routes/gamesRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import {getGames,postGames} from '../controllers/gamesController.js'
-import {validateGame} from '../middlewares/validateGame.js'
-const gamesRouter = Router();
-
-gamesRouter.get('/games',getGames);
-gamesRouter.post('/games',validateGame,postGames);
-export default gamesRouter;
